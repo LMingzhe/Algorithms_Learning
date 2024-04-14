91b9bc63daa8daf5d2b3c3ef38569f3d2f59f2d1
LMingzhe <15768835224@163.com>
2024-04-14 19:32:25 +0800

    permultations.cpp

---
+++ 回朔/permutations.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+/// leetcode 46 全排列
+/// @brief 给定一个 没有重复 数字的序列，返回其所有可能的全排列。
+/// 时间复杂度: O(n!) 空间复杂度: O(n)
+class Solution
+{
+private:
+    vector<vector<int>> result;
+    vector<int> path;
+    vector<bool> used;
+
+    void backtracking(vector<int>& nums)
+    {
+        if (path.size() == nums.size())
+        {
+            result.push_back(path);
+            return;
+        }
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (used[i] == true) // 去除深度上的重复，path中不含重复元素
+            {
+                continue;
+            }
+            path.push_back(nums[i]);
+            used[i] = true;
+            backtracking(nums);
+            path.pop_back();
+            used[i] = false;
+        }
+    }
+
+public:
+    vector<vector<int>> permute(vector<int>& nums)
+    {
+        result.clear();
+        path.clear();
+        used.resize(nums.size(), false);
+        backtracking(nums);
+        return result;
+    }
+};
