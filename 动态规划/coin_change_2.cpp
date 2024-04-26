9916a7c6e0d1c277f6c59d312a9194d85d59ed93
Liu Mingzhe <15768835224@163.com>
2024-04-26 09:15:25 +0800

    update

--- 动态规划/coin_change_2.cpp
+++ 动态规划/coin_change_2.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using std::vector;
-using std::max;
 
 /// leetcode 518 零钱兑换2
 /// @brief 给定不同面额的硬币和一个总金额。写出函数来计算可以凑成总金额的硬币组合数。假设每一种面额的硬币有无限个。
