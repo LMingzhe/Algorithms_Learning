f99cc1437c6012c51fc9a956582fd775e998f857
Liu Mingzhe <15768835224@163.com>
2024-05-15 09:14:51 +0800

    update

--- oppo23_testB/assemble_phone.cpp
+++ oppo23_testB/assemble_phone.cpp
@@ -2,6 +2,7 @@
 #include <limits>
 #include <algorithm>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 using std::vector;
@@ -12,27 +13,40 @@ using std::sort;
 using std::max;
 using std::min;
 
+/// @brief 哈希表题
+
 class Solution
 {
 public:
     int getMaxCount(vector<int>& cases, vector<int>& parts)
     {
-        std::unordered_map<int, int> map;
-        for (int i = 0; i < cases.size(); i++)
+        std::unordered_set<int> set;
+        int n = cases.size();
+        for (int i = 0; i < n; i++) // 使用set保存所有可能的售价
         {
-            for (int j = 0; j < parts.size(); j++)
+            for (int j = 0; j < n; j++)
             {
-                if (map.count(cases[i] + parts[j]))
-                {
-                    map[cases[i] + parts[j]]++;
-                }
-                else map[cases[i] + parts[j]] = 1;
+                set.insert(cases[i] + parts[j]);
             }
         }
-        int result = std::numeric_limits<int>::min();
-        for (const auto& pair : map)
+        int result = 0;
+        for (const auto& sum : set)
         {
-            if (pair.second > result) result = pair.second;
+            std::unordered_map<int, int> map;
+            int cnt = 0;
+            // map-key 为价格为 ai 的手机壳对应价格为 bi 的手机零件，售价为sum
+            // map-value 为完成所有key的组装所需的手机零件个数
+            for (int i = 0; i < n; i++) map[sum - cases[i]]++;
+            for (int j = 0; j < n; j++)
+            {
+                // 对应的价格的零件数量大于0，即可完成一个手机的组装
+                if (map.count(parts[j]) && map[parts[j]] > 0)
+                {
+                    cnt++;
+                    map[parts[j]]--; // 不可重复用，数量要-1
+                }
+            }
+            result = max(result, cnt);
         }
         return result;
     }
