aaa05574f36a6f4e5017364cc2a2d027436fd6d7
Liu Mingzhe <15768835224@163.com>
2024-05-16 16:16:48 +0800

    add ACM_template.cpp

---
+++ ACM_template.cpp
@@ -0,0 +1,48 @@
+/**
+ * @file ACM_template.cpp
+ * @author Azhe (you@domain.com)
+ * @brief ACM模式下的刷题模板，包含一些常用的库和函数，熟记
+ * @version 0.1
+ * @date 2024-05-16
+ * 
+ * @copyright Copyright (c) 2024 Azhe
+ * 
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <numeric>
+#include <limits>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+
+/// @brief 输出出入类
+using std::cout;
+using std::cin;
+using std::endl;
+
+/// @brief 常用算法函数库 <algorithm> <numeric> <limits>
+using std::min;
+using std::max;
+using std::sort;
+using std::swap;
+using std::reverse;
+using std::accumulate;
+using std::numeric_limits;
+
+/// @brief 容器类 <vector> <string> <unordered_map> <unordered_set> <stack> <queue>
+using std::vector;
+using std::string;
+using std::unordered_map; // 无序 效率高
+using std::unordered_set; // 无序 效率高、
+using std::map; // 有序
+using std::set; // 有序
+using std::stack;
+using std::queue;
