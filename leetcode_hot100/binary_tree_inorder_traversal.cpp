1886f624ac767083f24780943e1abdbe64277402
Liu Mingzhe <15768835224@163.com>
2024-05-20 09:05:56 +0800

    update

--- leetcode_hot100/binary_tree_inorder_traversal.cpp
+++ leetcode_hot100/binary_tree_inorder_traversal.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 #include "TreeNode_def.h"
 
 using std::vector;
+using std::stack;
 
 /// leetcode 94 二叉树中序遍历
 /// @brief 给定一个二叉树的根节点 root ，返回 它的 中序 遍历。
 
+// 递归
 class Solution
 {
 private:
@@ -23,6 +26,35 @@ public:
     {
         if (root == nullptr) return {};
         traversal(root);
+        return result;
+    }
+};
+
+// 迭代
+class Solution
+{
+public:
+    vector<int> inorderTraversal(TreeNode* root)
+    {
+        stack<TreeNode*> st;
+        vector<int> result;
+        if (root == nullptr) return result;
+        TreeNode* cur = root;
+        while (cur != nullptr || !st.empty())
+        {
+            if (cur != nullptr)
+            {
+                st.push(cur);
+                cur = cur->left;  // 左
+            } 
+            else 
+            {
+                cur = st.top();
+                st.pop();
+                result.push_back(cur->val); // 中
+                cur = cur->right; // 右
+            }
+        }
         return result;
     }
 };
---
+++ leetcode_hot100/symmetric-tree.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#include "TreeNode_def.h"
+
+using std::stack;
+using std::queue;
+
+/// 
+
+// 递归法
+class Solution
+{
+public:
+    bool compare(TreeNode* left, TreeNode* right)
+    {
+        if (left == nullptr && right != nullptr) return false;
+        else if (left != nullptr && right == nullptr) return false;
+        else if (left == nullptr && right == nullptr) return true;
+        else if (left->val != right->val) return false;
+
+        bool ouside = compare(left->left, right->right);
+        bool inside = compare(left->right, right->left);
+        bool isSame = ouside && inside;
+
+        return isSame;
+    }
+    bool isSymmetric(TreeNode* root)
+    {
+        if (root == nullptr) return true;
+        return compare(root->left, root->right);
+    }
+};
+
+// 迭代法 用队列实现
+class Solution
+{
+public:
+    bool isSymmetric(TreeNode* root)
+    {
+        if (root == nullptr) return true;
+        queue<TreeNode*> que;
+        que.push(root->left);
+        que.push(root->right);
+
+        while (!que.empty())
+        {
+            // 两两比较
+            TreeNode* leftNode = que.front();
+            TreeNode* rightNode = que.front();
+            que.pop(); que.pop();
+            // 左节点为空、右节点为空，此时说明是对称的
+            if (leftNode == nullptr && rightNode == nullptr) continue;
+            // 左右一个节点不为空，或者都不为空但数值不相同，返回false
+            if (leftNode == nullptr || rightNode == nullptr || leftNode->val != rightNode->val) return false;
+            que.push(leftNode->left);
+            que.push(rightNode->right);
+            que.push(leftNode->right);
+            que.push(rightNode->left);
+        }
+
+        return true;
+    }
+};
+
+// 迭代法 用栈实现
+class Solution  
+{
+public:
+    bool isSymmetric(TreeNode* root)
+    {
+        if (root == nullptr) return true;
+        stack<TreeNode*> st;
+        st.push(root->left);
+        st.push(root->right);
+
+        while (!st.empty())
+        {
+            // 两两比较
+            TreeNode* leftNode = st.top();
+            TreeNode* rightNode = st.top();
+            st.pop(); st.pop();
+            // 左节点为空、右节点为空，此时说明是对称的
+            if (leftNode == nullptr && rightNode == nullptr) continue;
+            // 左右一个节点不为空，或者都不为空但数值不相同，返回false
+            if (leftNode == nullptr || rightNode == nullptr || leftNode->val != rightNode->val) return false;
+            st.push(leftNode->left);
+            st.push(rightNode->right);
+            st.push(leftNode->right);
+            st.push(rightNode->left);
+        }
+
+        return true;
+    }
+};
