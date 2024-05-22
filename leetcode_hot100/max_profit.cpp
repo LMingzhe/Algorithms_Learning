fe05f4f91a70e4f33e7590bd4f01bd9429b60269
Liu Mingzhe <15768835224@163.com>
2024-05-22 17:28:26 +0800

    update

--- leetcode_hot100/max_profit.cpp
+++ leetcode_hot100/max_profit.cpp
@@ -10,7 +10,7 @@ using std::max;
 ///        你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
 ///        返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
 
-// 时间复杂度O(n) 空间复杂度O(1)
+// 时间复杂度O(n) 空间复杂度O(n)
 class Solution
 {
 public:
@@ -28,5 +28,24 @@ public:
             dp[i][1] = max(dp[i - 1][1], -prices[i]);
         }
         return dp[n - 1][0];
+    }
+};
+
+// 时间复杂度O(n) 空间复杂度O(1)
+class Solution
+{
+public:
+    int maxProfit(vector<int>& prices)
+    {
+        int n = prices.size();
+        vector<vector<int>> dp(2, vector<int>(2));
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < n; i++)
+        {
+            dp[i % 2][0] = max(dp[(i - 1) % 2][0], dp[(i - 1) % 2][1] + prices[i]);
+            dp[i % 2][1] = max(dp[(i - 1) % 2][1], -prices[i]);
+        }
+        return dp[(n - 1) % 2][0];
     }
 };
