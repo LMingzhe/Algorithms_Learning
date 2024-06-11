f828bc76ab0af39f6bfeadb7806376f537ce8980
LMingzhe <15768835224@163.com>
2024-06-11 23:00:12 +0800

    update

--- 用友23_test/software_construct.cpp
+++ 用友23_test/software_construct.cpp
@@ -5,14 +5,13 @@
 using namespace std;
 
 /// @brief 拓扑排序
-// fixme 这道题无法全部AC，不知道为啥，找了很久没找出问题在哪
 
 int main(int argc, char const *argv[])
 {
     int fileNums, depends;
     cin >> fileNums >> depends;
     vector<int> inDegree(fileNums, 0); // 记录每个节点的入度数
-    vector<vector<int>> edges(fileNums, vector<int>()); // 记录边，即依赖关系
+    vector<vector<int>> edges(fileNums); // 记录边，即依赖关系
     while (depends--)
     {
         int S, T;
@@ -22,13 +21,13 @@ int main(int argc, char const *argv[])
     }
 
     queue<int> que;
-    int visited = 0; // 记录已访问节点个数
+    // int visited = 0; // 记录已访问节点个数
     for (int i = 0; i < fileNums; i++)
     {
         if (inDegree[i] == 0)
         {
             que.push(i);
-            visited++;
+            // visited++;
         }
     }
 
@@ -44,17 +43,17 @@ int main(int argc, char const *argv[])
             if (inDegree[v] == 0)
             {
                 que.push(v);
-                visited++;
+                // visited++;
             }
         }
     }
 
-    if (visited == fileNums)
+    if (result.size() == fileNums)
     {
         for (int i = 0; i < fileNums; i++)
         {
-            if (i == fileNums - 1) cout << i << endl;
-            else cout << i << " ";
+            if (i == fileNums - 1) cout << result[i] << endl;
+            else cout << result[i] << " ";
         }
     } else {
         cout << -1 << endl;
